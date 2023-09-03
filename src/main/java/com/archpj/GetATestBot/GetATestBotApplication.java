a4a64be36b16e1212133e07699e530e3f7ad0027
ArchPJ <washubrnfg85@gmail.com>
2023-09-03 20:06:28 +0300

    03.09.2023 20:06 logger added

--- src/main/java/com/archpj/GetATestBot/GetATestBotApplication.java
+++ src/main/java/com/archpj/GetATestBot/GetATestBotApplication.java
@@ -1,10 +1,13 @@
 package com.archpj.GetATestBot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GetATestBotApplication {
+	static final Logger logger = LoggerFactory.getLogger(GetATestBotApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(GetATestBotApplication.class, args);
