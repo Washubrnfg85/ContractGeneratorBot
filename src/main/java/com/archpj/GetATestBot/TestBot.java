685e1263a1b017c65fc1f137b26dc598b8297d45
ArchPJ <washubrnfg85@gmail.com>
2023-09-15 03:20:19 +0300

    15.09.2023 3:19 main logic pased to Session class

--- src/main/java/com/archpj/GetATestBot/TestBot.java
+++ src/main/java/com/archpj/GetATestBot/TestBot.java
@@ -1,53 +1,34 @@
 package com.archpj.GetATestBot;
 
-import com.archpj.GetATestBot.components.Buttons;
-import com.archpj.GetATestBot.components.MenuOfSpecs;
-import com.archpj.GetATestBot.components.CommonSpecialisations;
 import com.archpj.GetATestBot.config.BotConfig;
-import com.archpj.GetATestBot.models.QuizResult;
-import com.archpj.GetATestBot.models.Quiz;
-import com.archpj.GetATestBot.services.QuizResultsService;
-import com.archpj.GetATestBot.services.QuizService;
+import com.archpj.GetATestBot.models.Session;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.archpj.GetATestBot.components.BotCommands.COMMANDS;
-import static com.archpj.GetATestBot.components.BotCommands.HELP_TEXT;
 
 @Component
 @Slf4j
 public class TestBot extends TelegramLongPollingBot {
 
-    final BotConfig botConfig;
-    private String specialisation;
-    private Quiz quiz = null;
-    private QuizResult quizResult;
-    private int iterationThroughTest = 0;
-    private String employeeAnswers = "";
+    private final BotConfig botConfig;
+    private List<Session> sessions;
 
-    @Autowired
-    private QuizService quizService;
-    @Autowired
-    private QuizResultsService quizResultsService;
-
-
-    public TestBot (BotConfig botConfig) {  // свериться с документацие (нужен ли при этом botConfig и @Configuration
+    public TestBot(BotConfig botConfig) {  // свериться с документацие (нужен ли при этом botConfig и @Configuration
         this.botConfig = botConfig;
+        this.sessions = new ArrayList<>();
         try {
             this.execute(new SetMyCommands(COMMANDS, new BotCommandScopeDefault(), null));
-        } catch (TelegramApiException e){
+        } catch (TelegramApiException e) {
             log.error(e.getMessage());
         }
     }
@@ -61,173 +42,67 @@ public class TestBot extends TelegramLongPollingBot {
     }  // depricated????
 
 
-    public void onUpdateReceived(Update update) {   // возможно использовать базу активней, напрашивается паттерн State
-        long adminTelegramId = botConfig.getAdminTelegramId();
+    public void onUpdateReceived(Update update) {
+        System.out.println("Method works");
 
-        if (update.hasMessage()) {
+        long employeeId = update.getMessage().getFrom().getId();
+        String employeeName = update.getMessage().getFrom().getLastName() != null ?
+                update.getMessage().getFrom().getFirstName() + " " + update.getMessage().getFrom().getLastName() :
+                update.getMessage().getFrom().getFirstName();
 
-            Message incomingMessage = update.getMessage();
-            long chatId = update.getMessage().getChatId();
-            SendMessage message;
+        SendMessage sendMessage;
 
-            if (incomingMessage.getText().equals("/start")) {
-                iterationThroughTest = 0;
-                employeeAnswers = "";
-
-                message = SendMessage.builder().
-                        chatId(chatId).
-                        text("Желаете начать тест?").
-                        replyMarkup(Buttons.suggestTest()).
-                        build();
-
-            } else if (incomingMessage.getText().equals("/test")) {
-                iterationThroughTest = 0;
-                employeeAnswers = "";
-
-                message = SendMessage.builder().
-                        chatId(chatId).
-                        text("Выбирайте тему:").
-                        replyMarkup(MenuOfSpecs.sendMenu()).
-                        build();
-
-            } else if (incomingMessage.getText().equals("/help")) {
-                message = SendMessage.builder().
-                        chatId(chatId).
-                        text(HELP_TEXT).
-                        build();
-            } else if (CommonSpecialisations.toList().contains(incomingMessage.getText())) {
-
-                specialisation = incomingMessage.getText();
-                iterationThroughTest = 0;
-                employeeAnswers = "";
-
-                quiz = new Quiz(quizService, specialisation);
-
-                message = SendMessage.builder().
-                        chatId(chatId).
-                        text(quiz.getQuestions().get(iterationThroughTest)).
-                        replyMarkup(Buttons.suggestAnswers()).
-                        build();
-                iterationThroughTest++;
-
-            } else {
-                message = SendMessage.builder().
-                        chatId(chatId).text("""
-                                Вы что-то не то делайте.
-                                ("Нормально делай - нормально будет!
-                                Сократ 429 год до н.э.")
-                                Если возникли трудности, нажмите "/help\"""").
-                        build();
-            }
-
-            try {
-                execute(message);
-            } catch (TelegramApiException e) {
-                throw new RuntimeException(e);
-            }
-
-        } else if (update.hasCallbackQuery()) {
-            CallbackQuery callbackQuery = update.getCallbackQuery();
-
-            String employeeName = callbackQuery.getFrom().getFirstName() + " " + callbackQuery.getFrom().getLastName();
-            long employeeTelegramId = callbackQuery.getFrom().getId();
-            long chatId = callbackQuery.getMessage().getChatId();
-
-            SendMessage message;
-
-            if (callbackQuery.getData().equals("/test rejected")) {
-                message = SendMessage.builder().
-                        chatId(chatId).
-                        text("Ок.\nВы можете начать тест позднее выбрав пункт меню \"/test\"").
-                        build();
-                //Add sending of picture functional
-
-            } else if (callbackQuery.getData().equals("/test")) {
-                iterationThroughTest = 0;
-                employeeAnswers = "";
-
-                message = SendMessage.builder().
-                        chatId(chatId).
-                        text("Выбирайте тему:").
-                        replyMarkup(MenuOfSpecs.sendMenu()).
-                        build();
-
-            } else if (callbackQuery.getData().equals("A") ||
-                    callbackQuery.getData().equals("B") ||
-                    callbackQuery.getData().equals("C") ||
-                    callbackQuery.getData().equals("D")) {
-
-                employeeAnswers += callbackQuery.getData();
-                if (iterationThroughTest < quiz.getQuestions().size()) {
-
-                    message = SendMessage.builder().
-                            chatId(chatId).
-                            text(quiz.getQuestions().get(iterationThroughTest)).
-                            replyMarkup(Buttons.suggestAnswers()).
-                            build();
-                    iterationThroughTest++;
-
-                } else {
-                    quiz.setEmployeeAnswers(employeeAnswers);
-                    iterationThroughTest = 0;
-                    employeeAnswers = "";
-
-                    quizResult = new QuizResult(employeeTelegramId,
-                            employeeName,
-                            quiz.getSpecialisation(),
-                            quiz.getCorrectAnswer() + "\n" + quiz.getEmployeeAnswers(),
-                            new Timestamp(System.currentTimeMillis()));
-                    quizResult.calculateEmployeeScore();
-                    quizResultsService.saveQuizResult(quizResult);
-
-//                    На данный момент востребованность данного функционала под вопросом
-//                    if (quizResultsService.checkIfPresents(quizResult)) {
-//                        quizResultsService.updateQuizResult(quizResult);
-//                    } else {
-//                        quizResultsService.saveQuizResult(quizResult);
-//                    }
-
-                    message = SendMessage.builder().
-                            chatId(chatId).
-                            text("Тест по теме " + specialisation + " завершен.\n" +
-                                    "Ваш результат: " + quizResult.getScore() +
-                                    "\nВы можете выбрать другую тему или пересдать эту.").
-                            build();
-
-                    sendResultToAdmin(adminTelegramId, employeeName);
-
-                }
-            } else {
-                message = SendMessage.builder().
-                        chatId(chatId).
-                        text("Query hadn't been processed").
-                        build();
-
-            }
-
-            AnswerCallbackQuery close = AnswerCallbackQuery.builder()
-                    .callbackQueryId(callbackQuery.getId()).build();
-
-            try {
-                execute(close);
-                execute(message);
-            } catch (TelegramApiException e) {
-                throw new RuntimeException(e);
-            }
+        if (sessions.isEmpty() || !employeeHasSession(employeeId)) {
+            System.out.println("Creating new Session");
+            Session session = new Session(employeeId, employeeName, update);
+            sendMessage = session.setUpdate(update);
+        } else {
+            sendMessage = passUpdateToSession(employeeId, update);
         }
-    }
-
-    public void sendResultToAdmin(Long adminTelegramId, String employeeName) {
-        SendMessage messageToAdmin= SendMessage.builder().
-                chatId(adminTelegramId).
-                text(employeeName + " прошел тест по теме " + specialisation + "\n" +
-                        "с результатом " + quizResult.getScore()).
-                build();
 
         try {
-            execute(messageToAdmin);
+            execute(sendMessage);
         } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException();
         }
+
+    }
+
+    public boolean employeeHasSession(long employeeId) {
+        for (Session session : sessions) {
+            if (session.getEmployeeId() == employeeId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public SendMessage passUpdateToSession(long employeeId, Update update) {
+        return sessions.stream().
+                filter(session -> employeeId == session.getEmployeeId()).
+                findAny().
+                get().
+                setUpdate(update);
     }
 }
+
+
+
+
+
+
+
+
+//    public void sendResultToAdmin(Long adminTelegramId, String employeeName) {
+//        SendMessage messageToAdmin= SendMessage.builder().
+//                chatId(adminTelegramId).
+//                text(employeeName + " прошел тест по теме " + specialisation + "\n" +
+//                        "с результатом " + quizResult.getScore()).
+//                build();
+//
+//        try {
+//            execute(messageToAdmin);
+//        } catch (TelegramApiException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
--- src/main/java/com/archpj/GetATestBot/models/Session.java
+++ src/main/java/com/archpj/GetATestBot/models/Session.java
@@ -1,19 +1,31 @@
 package com.archpj.GetATestBot.models;
 
+import com.archpj.GetATestBot.components.Buttons;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
 import java.util.List;
 
 public class Session {
+
+    private Update update;
+
     private long employeeId;
     private String employeeName;
     private List<String> questions;
     private String correctAnswers;
     private String employeeAnswers;
 
-    public Session(long employeeId, String employeeName, List<String> questions, String correctAnswers) {
+    private SendMessage message;
+
+    public Session(long employeeId, String employeeName, Update update) {
+        System.out.println("Session constructor working");
+
         this.employeeId = employeeId;
         this.employeeName = employeeName;
-        this.questions = questions;
-        this.correctAnswers = correctAnswers;
+        this.update = update;
     }
 
     public long getEmployeeId() {
@@ -35,4 +47,61 @@ public class Session {
     public String getEmployeeAnswers() {
         return employeeAnswers;
     }
+
+    public SendMessage setUpdate(Update update) {
+        System.out.println("setUpdate working");
+
+        this.update = update;
+        return handleUpdate();
+    }
+
+    public SendMessage handleUpdate() {
+        System.out.println("handleUpdate working");
+
+        if (update.hasMessage()) return handleMessage();
+        if (update.hasCallbackQuery()) return handleCallbackQuery();
+
+        return SendMessage.builder().
+                chatId(update.getMyChatMember().getChat().getId()).
+                text("Neither message nor callback.").
+                replyMarkup(Buttons.suggestTest()).
+                build();
+    }
+
+    private SendMessage handleMessage() {
+        System.out.println("handleMessage working");
+
+        Message incomingMessage = update.getMessage();
+        long chatId = update.getMessage().getChatId();
+        SendMessage message;
+
+        message = SendMessage.builder().
+                chatId(chatId).
+                text("Желаете начать тест?").
+                replyMarkup(Buttons.suggestTest()).
+                build();
+
+        return message;
+
+    }
+
+    private SendMessage handleCallbackQuery() {
+        System.out.println("handleCallbackQuery working");
+
+        CallbackQuery callbackQuery = update.getCallbackQuery();
+
+        long employeeTelegramId = callbackQuery.getFrom().getId();
+        long chatId = callbackQuery.getMessage().getChatId();
+
+        SendMessage message;
+
+        message = SendMessage.builder().
+                chatId(chatId).
+                text("Ок.\nВы можете начать тест позднее выбрав пункт меню \"/test\"").
+                build();
+
+        return message;
+    }
+
+    
 }
